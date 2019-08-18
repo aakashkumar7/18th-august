79f46dd398871d981697ec0be067aee93b2336ea
aakashkumar7 <aakashk9968@gmail.com>
2019-08-18 14:05:19 +0530

    greeter

--- file03.js
+++ file03.js
@@ -2,3 +2,9 @@ console.log("bye world");
 console.log("yes world");
 console.log("another commit");
 console.log("yet");
+function greeter(name)
+{
+    console.log("hello "+ name);
+
+}
+greeter("aakash");
